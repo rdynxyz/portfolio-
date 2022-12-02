e985f5e2dda109be7aacbf4795fbfebb0eee3087
rdynxyz <radyan6661@gmail.com>
2022-12-02 17:36:21 +0700

    styling cards

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <section class="bg-[#171717] text-[#A3ABB2] font-Poppins font-medium text-sm sm:text-[22px] sm:leading-8">
+  <section class="bg-[#171717] text-[#A3ABB2] font-Poppins font-medium text-sm sm:text-base sm:leading-8">
     <div class="max-w-[797px] m-auto px-5 pt-10 pb-10">
       <!-- Profile -->
       <div class="w-full flex flex-col sm:flex-row items-center justify-center gap-[40px]">
-        <div class="p-1 w-[215px] bg-white rounded-full">
+        <div class="p-1 w-[215px] bg-[#A3ABB2] rounded-full">
           <img :src="`./PP.jpg`" alt="" class="w-full rounded-full ">
         </div>
         <div class="flex flex-col items-center sm:items-start">
@@ -19,31 +19,41 @@
       <div class="mt-7 sm:mt-12 md:w-[634px] flex justify-center gap- sm:gap-[90px] py-5 m-auto">
         <div v-for="profile in profiles" :key="profile" class="">
           <h1 align="center" class="text-base sm:text-2xl">{{ profile.p1 }}</h1>
-          <h1 align="center" >{{ profile.p2 }}</h1>
+          <h1 align="center">{{ profile.p2 }}</h1>
         </div>
       </div>
       <!-- CV Contact button -->
-      <div class="mt-7 sm:mt-12 sm:w-[618px] flex justify-between m-auto">
+      <div class="mt-8 sm:w-[618px] flex justify-between m-auto">
         <a href="https://drive.google.com/file/d/1ACfXB9ZZkEVDf0WjpgsbvgDLq7h924Lk/view">
-          <button class="bg-white hover:bg-white/90 duration-100 text-[#171717] w-[158px] sm:w-[290px] h-[44px] sm:h-[80px] rounded-[10px]">Download CV</button>
+          <button
+            class="bg-white hover:bg-white/90 duration-100 text-[#171717] w-[158px] sm:w-[290px] h-[44px] sm:h-16 rounded-[10px]">Download
+            CV<i class="fa-solid fa-file-arrow-down ml-5"></i></button>
 
         </a>
         <a href="mailto:radyan6661@gmail.com ">
-          <button class="bg-[#242424] w-[158px] sm:w-[290px] h-[44px] sm:h-[80px] rounded-[10px]">Contact Me</button>
+          <button class="bg-[#242424] w-[158px] sm:w-[290px] h-[44px] sm:h-16 rounded-[10px]">Contact Me</button>
         </a>
       </div>
       <!-- Tabs button -->
-      <div class="w-full h-[116px] mt-12 sm:mt-20 bg-[#242424] rounded-[20px] p-[18px] flex justify-between">
-        <button class="h-full w-[350px] bg-[#171717] rounded-[10px]">Portfolio</button>
+      <div class="w-full h-[96px] mt-12 sm:mt-20 bg-[#242424] rounded-[20px] p-4 flex justify-between">
+        <button class="h-16 w-[350px] bg-[#171717] rounded-[10px]">Portfolio</button>
         <button v-on:click="clicked = !clicked" v-bind:class="{ 'bg-[#171717]': clicked, 'bg-transparent': !clicked }"
-          class="h-full w-[350px] rounded-[10px]">Skills</button>
+          class="h-16 w-[350px] rounded-[10px]">Skills</button>
       </div>
       <!-- Portfolio -->
-      <div class="mt-7 sm:mt-12 w-full grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div v-for="portfolio in portfolios" :key="portfolio" class="rounded-2xl h-[226px] overflow-hidden">
-          <a :href="`${portfolio.link}`">
-            <img :src="`${portfolio.img}`" alt="" class="rounded-2xl">
-          </a>
+      <div class="mt-8 w-full grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div v-for="portfolio in portfolios" :key="portfolio" class="bg-[#242424] rounded-2xl relative overflow-hidden">
+          <div class="group rounded-2xl h-[226px] overflow-hidden relative">
+            <img :src="`${portfolio.img}`" alt="" class="">
+          </div>
+          <div class=" w-full p-4 font-normal text-base">
+            <h1 class="text-2xl font-medium text-white">{{ portfolio.name }}</h1>
+            <h1 class="mt-3">Built with : {{ portfolio.built }}</h1>
+            <a :href="`${portfolio.link}`" class="relative rounded-2xl">
+              <button class="mt-3 underline hover:text-white ">View </button>
+            </a>
+          </div>
+
         </div>
       </div>
       <!-- Credit -->
@@ -65,10 +75,10 @@ export default {
       { p1: '50+', p2: 'Completed projects' },
       { p1: '20+', p2: 'Satisfied customers' },
     ]
-    let portfolios =[
-      {img : 'Salty.jpg', link : 'https://portfolio-rdynxyz.vercel.app'},
-      {img : 'Eunoia.jpg', link : 'https://rdynxyz.github.io/eunoia/'},
-      {img : 'Real State.jpg', link : 'https://realstate-rdynxyz.vercel.app'},
+    let portfolios = [
+      { img: 'Salty.jpg', link: 'https://salty-rdynxyz.vercel.app/', name: 'Salty - Travel Website', built: 'Vue Tailwind' },
+      { img: 'Eunoia.jpg', link: 'https://rdynxyz.github.io/eunoia/', name: 'Eunoia - Service Website', built: 'HTML JS Tailwind' },
+      { img: 'Real State.jpg', link: 'https://realstate-rdynxyz.vercel.app', name: 'Reis - Real State Website', built: 'HTML JS Tailwind' },
     ]
     return { medias, profiles, portfolios }
   }
