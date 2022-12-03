488f27fac7cc9b4e47b147622bbc3289227d6597
rdynxyz <radyan6661@gmail.com>
2022-12-03 10:00:38 +0700

    add simple projects

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@
         <a href="https://drive.google.com/file/d/1ACfXB9ZZkEVDf0WjpgsbvgDLq7h924Lk/view">
           <button
             class="bg-white hover:bg-white/90 duration-100 text-[#171717] w-[158px] sm:w-[290px] h-[44px] sm:h-16 rounded-[10px]">Download
-            CV<i class="fa-solid fa-file-arrow-down ml-5"></i></button>
+            CV<i class="fa-solid fa-file-arrow-down ml-2"></i></button>
 
         </a>
         <a href="mailto:radyan6661@gmail.com ">
@@ -42,8 +42,9 @@
       </div>
       <!-- Portfolio -->
       <div class="mt-8 w-full grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div v-for="portfolio in portfolios" :key="portfolio" class="bg-[#242424] rounded-2xl relative overflow-hidden">
-          <div class="group rounded-2xl h-[226px] overflow-hidden relative">
+        <!-- Project -->
+        <div v-for="portfolio in portfolios" :key="portfolio" class="bg-[#242424] rounded-2xl relative overflow-hidden border-white border-2">
+          <div class="group rounded-t-2xl h-[226px] overflow-hidden relative border-white border-b-2">
             <img :src="`${portfolio.img}`" alt="" class="">
           </div>
           <div class=" w-full p-4 font-normal text-base">
@@ -53,9 +54,24 @@
               <button class="mt-3 underline hover:text-white ">View </button>
             </a>
           </div>
-
         </div>
       </div>
+        <!-- Small Projects -->
+        <h1 class="mt-10"> <span class="text-white">#</span> Small-projects</h1>
+        <div class="mt-8 w-full grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div v-for="project in projects" :key="project" class="bg-[#242424] rounded-2xl relative overflow-hidden border-white border-2">
+          <div class="group rounded-t-2xl h-[226px] overflow-hidden relative border-white border-b-2">
+            <img :src="`${project.img}`" alt="" class="">
+          </div>
+          <div class=" w-full p-4 font-normal text-base">
+            <h1 class="text-2xl font-medium text-white">{{ project.name }}</h1>
+            <h1 class="mt-3">Built with : {{ project.built }}</h1>
+            <a :href="`${project.link}`" class="relative rounded-2xl">
+              <button class="mt-3 underline hover:text-white ">View </button>
+            </a>
+          </div>
+        </div>
+        </div>
       <!-- Credit -->
       <h1 class="text-center mt-32">© Radyan Bintang. 2022 All rigths reserved</h1>
     </div>
@@ -71,16 +87,21 @@ export default {
       { link: 'fa-brands fa-linkedin', ref: '' },
     ]
     let profiles = [
-      { p1: '-1 years', p2: 'Years of work experience' },
-      { p1: '50+', p2: 'Completed projects' },
-      { p1: '20+', p2: 'Satisfied customers' },
+      { p1: '1', p2: 'Year of work experience' },
+      { p1: '5+', p2: 'Completed web apps' },
+      { p1: '5+', p2: 'Small projects' },
     ]
     let portfolios = [
       { img: 'Salty.jpg', link: 'https://salty-rdynxyz.vercel.app/', name: 'Salty - Travel Website', built: 'Vue Tailwind' },
       { img: 'Eunoia.jpg', link: 'https://rdynxyz.github.io/eunoia/', name: 'Eunoia - Service Website', built: 'HTML JS Tailwind' },
       { img: 'Real State.jpg', link: 'https://realstate-rdynxyz.vercel.app', name: 'Reis - Real State Website', built: 'HTML JS Tailwind' },
+      { img: 'Portfolio.jpg', link: 'https://portfolio-rdynxyz.vercel.app', name: 'Portfolio - You’re using it rn', built: 'Vue Tailwind' },
     ]
-    return { medias, profiles, portfolios }
+    let projects = [
+      { img: 'tic-tac-toe.jpg', link: 'https://tic-tac-toe-rdynxyz.vercel.app/', name: 'Tic Tac Toe Game', built: 'Vue Tailwind' },
+      { img: 'Real State.jpg', link: 'https://simple-calculator-rdynxyz.vercel.app', name: 'Simple Calculator', built: 'HTML JS' },
+    ]
+    return { medias, profiles, portfolios, projects }
   }
 }
 </script>
